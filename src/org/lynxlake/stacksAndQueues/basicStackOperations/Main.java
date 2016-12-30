9dad3f7fca8d8ce43e27259cfa7ae933672ca1de
apple <1>
2016-12-30 22:32:01 +0200

    Initial commit

---
+++ src/org/lynxlake/stacksAndQueues/basicStackOperations/Main.java
@@ -0,0 +1,7 @@
+package org.lynxlake.stacksAndQueues.basicStackOperations;
+
+/**
+ * Created by apple on 12/30/16.
+ */
+public class Main {
+}
---
+++ src/org/lynxlake/stacksAndQueues/reverseNumbers/Main.java
@@ -0,0 +1,8 @@
+// https://judge.softuni.bg/Contests/Practice/Index/187#0
+package org.lynxlake.stacksAndQueues.reverseNumbers;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
