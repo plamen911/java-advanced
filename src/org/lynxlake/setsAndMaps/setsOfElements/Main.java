d8f9e92e888c5619375fced81edba9ac3cc1ba6f
apple <1>
2017-01-01 12:48:32 +0200

    Create Sets Of Elements

---
+++ src/org/lynxlake/setsAndMaps/setsOfElements/Main.java
@@ -0,0 +1,39 @@
+// https://judge.softuni.bg/Contests/Practice/Index/188#1
+package org.lynxlake.setsAndMaps.setsOfElements;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] params = input.nextLine().trim().split("\\s+");
+        int n = Integer.valueOf(params[0]);
+        int m = Integer.valueOf(params[1]);
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            set.add(input.nextInt());
+        }
+        for (int i = 0; i < m; i++) {
+            set2.add(input.nextInt());
+        }
+
+        set.retainAll(set2);
+
+        System.out.printf("%s", join(set, " "));
+    }
+
+    private static String join(Collection<?> s, String delimiter) {
+        StringBuilder builder = new StringBuilder();
+        Iterator<?> iter = s.iterator();
+        while (iter.hasNext()) {
+            builder.append(iter.next());
+            if (!iter.hasNext()) {
+                break;
+            }
+            builder.append(delimiter);
+        }
+        return builder.toString();
+    }
+}
---
+++ src/org/lynxlake/stacksAndQueues/recursiveFibonacci/Main.java
@@ -0,0 +1,8 @@
+// https://judge.softuni.bg/Contests/Practice/Index/187#7
+package org.lynxlake.stacksAndQueues.recursiveFibonacci;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
