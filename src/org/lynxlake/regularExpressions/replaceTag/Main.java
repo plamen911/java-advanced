19ed12f80bd67f00b2c498aa1ced8f6bbd3c70d7
apple <1>
2017-01-04 21:12:47 +0200

    Replace a tag improvements

--- src/org/lynxlake/regularExpressions/replaceTag/Main.java
+++ src/org/lynxlake/regularExpressions/replaceTag/Main.java
@@ -4,6 +4,8 @@ package org.lynxlake.regularExpressions.replaceTag;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,9 +18,31 @@ public class Main {
             line = reader.readLine();
         }
 
-        String result = builder.toString().replaceAll("<a", "[URL");
-        result = result.replaceAll("<\\/a>", "[\\/URL]");
+        String regex = "(<a) .*?(>)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(builder.toString());
 
+        String result = builder.toString();
+
+        while (matcher.find()) {
+            String openingTag = matcher.group(1);
+            String closingBracket = matcher.group(2);
+
+            int startIndex = matcher.start();
+            int endIndex = matcher.end();
+
+            String firstPart = result.substring(0, startIndex);
+            String match = result.substring(startIndex, endIndex);
+            String lastPart = result.substring(endIndex);
+
+            match = match.replace(openingTag, "[URL");
+            // match = match.replace(closingBracket, "]");
+
+            result = firstPart + match + lastPart;
+        }
+
+        // String result = builder.toString().replaceAll("<a", "[URL");
+        result = result.replace("</a>", "[/URL]");
         System.out.println(result);
     }
 }
