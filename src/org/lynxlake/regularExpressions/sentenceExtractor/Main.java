0223f6bc3d0946d1de6e8058e8bfb9bd6076690d
apple <1>
2017-01-04 21:41:10 +0200

    Create Sentence Extractor

---
+++ src/org/lynxlake/regularExpressions/sentenceExtractor/Main.java
@@ -0,0 +1,26 @@
+// https://judge.softuni.bg/Contests/Practice/Index/189#5
+package org.lynxlake.regularExpressions.sentenceExtractor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String keyword = reader.readLine().trim();
+        String text = reader.readLine().trim();
+
+        // String regexp = "[^!?.]+[!?.]";
+        // [^!?.]+\bis\b[^!?.]+[!?.]
+        String regexp = "[^!?.]+" + "\\b" + keyword + "\\b" + "[^!?.]+[!?.]";
+        Pattern pattern = Pattern.compile(regexp);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group(0));
+        }
+    }
+}
