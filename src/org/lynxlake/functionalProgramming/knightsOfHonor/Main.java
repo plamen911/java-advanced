16f3d852ed1676aab21077a2a8b760fb72c78542
apple <1>
2017-01-09 00:03:47 +0200

    Create Knights of Honor

---
+++ src/org/lynxlake/functionalProgramming/knightsOfHonor/Main.java
@@ -0,0 +1,16 @@
+// https://judge.softuni.bg/Contests/Practice/Index/194#1
+package org.lynxlake.functionalProgramming.knightsOfHonor;
+
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] names = scanner.nextLine().trim().split("\\s+");
+        Consumer<String> print = name -> System.out.printf("Sir %s%n", name);
+        for (String name : names) {
+            print.accept(name);
+        }
+    }
+}
