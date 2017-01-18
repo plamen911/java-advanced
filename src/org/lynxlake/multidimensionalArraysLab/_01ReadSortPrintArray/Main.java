4893880b6a1ddc0b85bacdea8c5496b18d282dd8
apple <1>
2017-01-18 10:08:37 +0200

    ReadSortPrintArray

---
+++ src/org/lynxlake/multidimensionalArraysLab/_01ReadSortPrintArray/Main.java
@@ -0,0 +1,23 @@
+// https://judge.softuni.bg/Contests/Practice/Index/381#0
+package org.lynxlake.multidimensionalArraysLab._01ReadSortPrintArray;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = Integer.parseInt(scanner.nextLine());
+        String[] names = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            names[i] = scanner.nextLine();
+        }
+
+        Arrays.sort(names);
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
