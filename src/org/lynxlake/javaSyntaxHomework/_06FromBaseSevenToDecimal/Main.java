b2766ba30a2e1fee2b77d393a2b3b2a1fbab42b3
apple <1>
2017-01-20 17:30:23 +0200

    Create 06. From Base Seven To Decimal

---
+++ src/org/lynxlake/javaSyntaxHomework/_06FromBaseSevenToDecimal/Main.java
@@ -0,0 +1,27 @@
+// https://judge.softuni.bg/Contests/Practice/Index/383#5
+package org.lynxlake.javaSyntaxHomework._06FromBaseSevenToDecimal;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        System.out.printf("%d", fromBaseSeven(n));
+    }
+
+    private static String toBaseSeven(long number) {
+        int baseToConvertTo = 7;
+        return Long.toString(number, baseToConvertTo);
+    }
+
+    private static long fromBaseSeven(long number) {
+        long i = number;
+        while (true) {
+            if (Integer.parseInt(toBaseSeven(i)) == number) {
+                return i;
+            }
+            i--;
+        }
+    }
+}
