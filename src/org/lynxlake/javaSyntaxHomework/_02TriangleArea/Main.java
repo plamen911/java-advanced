0f389fd0031c549097c80388c0bc168d99bff5e0
apple <1>
2017-01-20 15:57:24 +0200

    Create Triangle Area

--- src/org/lynxlake/javaSyntaxHomework/_02TriangleArea/Main.java
+++ src/org/lynxlake/javaSyntaxHomework/_02TriangleArea/Main.java
@@ -1,7 +1,20 @@
+// https://judge.softuni.bg/Contests/Practice/Index/383#1
 package org.lynxlake.javaSyntaxHomework._02TriangleArea;
 
-/**
- * Created by Plamen Markov on 1/20/17.
- */
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long ax = scanner.nextLong();
+        long ay = scanner.nextLong();
+        long bx = scanner.nextLong();
+        long by = scanner.nextLong();
+        long cx = scanner.nextLong();
+        long cy = scanner.nextLong();
+
+        double area = Math.abs((ax * (by - cy) + bx * (cy - ay) + cx * (ay - by)) / 2);
+
+        System.out.printf("%.0f", area);
+    }
 }
