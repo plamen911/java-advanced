d29f0f64309c59288848886012538c552b2a0bdb
apple <1>
2017-01-20 16:17:15 +0200

    Create Formatting Numbers

---
+++ src/org/lynxlake/javaSyntaxHomework/_03FormattingNumbers/Main.java
@@ -0,0 +1,21 @@
+// https://judge.softuni.bg/Contests/Practice/Index/383#2
+package org.lynxlake.javaSyntaxHomework._03FormattingNumbers;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        System.out.printf("|%-10X|%010d|%10.2f|%-10.3f|%n",
+                a,
+                new BigInteger(Integer.toBinaryString(a)),
+                b,
+                c
+        );
+    }
+}
