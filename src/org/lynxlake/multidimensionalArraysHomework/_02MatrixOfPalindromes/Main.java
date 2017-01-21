fbc61f2b3c6986e36ba372ffad6c574cead33442
apple <1>
2017-01-21 22:02:32 +0200

    Create 02. MatrixOfPalindromes

--- src/org/lynxlake/multidimensionalArraysHomework/_02MatrixOfPalindromes/Main.java
+++ src/org/lynxlake/multidimensionalArraysHomework/_02MatrixOfPalindromes/Main.java
@@ -1,8 +1,26 @@
 // https://judge.softuni.bg/Contests/Practice/Index/387#1
 package org.lynxlake.multidimensionalArraysHomework._02MatrixOfPalindromes;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        Scanner scanner = new Scanner(System.in);
+        int r = scanner.nextInt();
+        int c = scanner.nextInt();
+        String[][] matrix = new String[r][c];
 
+        for (int i = 0; i < r; i++) {
+            int k = 0;
+            for (int j = i; j < c + i; j++) {
+                String palindrome = String.format("%s%s%s", alphabet[i], alphabet[j], alphabet[i]);
+                matrix[i][k++] = palindrome;
+            }
+        }
+
+        for (String[] strings : matrix) {
+            System.out.printf("%s%n", String.join(" ", strings));
+        }
     }
 }
