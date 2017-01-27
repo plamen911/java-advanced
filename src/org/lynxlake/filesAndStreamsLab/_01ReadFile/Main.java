d407d8944bc2b5350152606b43d1c51e843fda6f
apple <1>
2017-01-27 14:34:47 +0200

    Create 02. Write to a File

--- src/org/lynxlake/filesAndStreamsLab/_01ReadFile/Main.java
+++ src/org/lynxlake/filesAndStreamsLab/_01ReadFile/Main.java
@@ -7,12 +7,14 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         String path = "src/org/lynxlake/filesAndStreamsLab/resources/input.txt";
+
         try (FileInputStream fileStream = new FileInputStream(path)) {
             int oneByte = fileStream.read();
-            while (oneByte > 0) {
+            while (oneByte >= 0) {
                 System.out.printf("%s ", Integer.toBinaryString(oneByte));
                 oneByte = fileStream.read();
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/org/lynxlake/filesAndStreamsLab/_02WriteToAFile/Main.java
+++ src/org/lynxlake/filesAndStreamsLab/_02WriteToAFile/Main.java
@@ -1,7 +1,40 @@
+// https://judge.softuni.bg/Contests/Practice/Index/403#1
 package org.lynxlake.filesAndStreamsLab._02WriteToAFile;
 
-/**
- * Created by Plamen Markov on 1/27/17.
- */
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        String path = "src/org/lynxlake/filesAndStreamsLab/resources/input.txt";
+        String outputPath = "src/org/lynxlake/filesAndStreamsLab/resources/output.txt";
+        List<Character> punctuation = new ArrayList<>();
+        punctuation.add(',');
+        punctuation.add('.');
+        punctuation.add('!');
+        punctuation.add('?');
+
+        try (FileInputStream fileStream = new FileInputStream(path)) {
+
+            FileOutputStream fileOutputStream = new FileOutputStream(outputPath);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, StandardCharsets.ISO_8859_1);
+            Writer out = new BufferedWriter(outputStreamWriter);
+
+            int oneByte = fileStream.read();
+            while (oneByte >= 0) {
+                if (!punctuation.contains((char)oneByte)) {
+                    out.write(oneByte);
+                }
+                oneByte = fileStream.read();
+            }
+
+            out.close();
+            fileStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
