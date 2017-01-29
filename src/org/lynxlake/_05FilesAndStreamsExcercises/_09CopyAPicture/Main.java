222f7fc8a85fd03fb5d8b2daee2d676fd48206fd
apple <1>
2017-01-30 00:42:26 +0200

    Create 9. Copy a Picture

---
+++ src/org/lynxlake/_05FilesAndStreamsExcercises/_09CopyAPicture/Main.java
@@ -0,0 +1,24 @@
+package org.lynxlake._05FilesAndStreamsExcercises._09CopyAPicture;
+
+import java.io.*;
+
+public class Main {
+    private static final String PROJECT_PATH = System.getProperty("user.dir") + "/src/resources/";
+    private static final String FILE_PATH_IN = PROJECT_PATH + "picture.jpg";
+    private static final String FILE_PATH_OUT = PROJECT_PATH + "picture-copy.jpg";
+
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream(FILE_PATH_IN);
+             FileOutputStream fos = new FileOutputStream(FILE_PATH_OUT)) {
+
+            int oneByte = fis.read();
+            while (oneByte >= 0) {
+                fos.write(oneByte);
+                oneByte = fis.read();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
