28c5937b74ddafe97519c32320b6ca45d794fd72
apple <1>
2017-01-29 09:27:27 +0200

    Create 02. Simple Calculator

--- src/org/lynxlake/stacksAndQueuesLab/_02SimpleCalculator/Main.java
+++ src/org/lynxlake/stacksAndQueuesLab/_02SimpleCalculator/Main.java
@@ -1,7 +1,34 @@
+// https://judge.softuni.bg/Contests/Practice/Index/386#1
 package org.lynxlake.stacksAndQueuesLab._02SimpleCalculator;
 
-/**
- * Created by Plamen Markov on 1/28/17.
- */
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] tokens = scanner.nextLine().trim().split("\\s+");
+
+        Deque<String> stack = new ArrayDeque<>();
+        Collections.addAll(stack, tokens);
+
+        while (stack.size() > 1) {
+            int first = Integer.valueOf(stack.pop());
+            String op = stack.pop();
+            int second = Integer.valueOf(stack.pop());
+
+            switch (op) {
+                case "+":
+                    stack.push(String.valueOf(first + second));
+                    break;
+                case "-":
+                    stack.push(String.valueOf(first - second));
+                    break;
+            }
+        }
+
+        System.out.println(stack.pop());
+    }
 }
