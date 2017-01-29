0b17b3a92ec1fce9bb5c04f4ab0f4b782776ec34
apple <1>
2017-01-30 00:57:30 +0200

    Create 10. Serialize Array List

---
+++ src/org/lynxlake/_05FilesAndStreamsExcercises/_10SerializeArrayList/Main.java
@@ -0,0 +1,24 @@
+package org.lynxlake._05FilesAndStreamsExcercises._10SerializeArrayList;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    private static final String PROJECT_PATH = System.getProperty("user.dir") + "/src/resources/";
+    private static final String FILE_PATH = PROJECT_PATH + "doubles.list";
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        List<Double> doubles = new ArrayList<>();
+        Collections.addAll(doubles, 5.6, 45.6, 3.46);
+
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH));
+        oos.writeObject(doubles);
+        oos.flush();
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH));
+        System.out.println(ois.readObject());
+        ois.close();
+    }
+}
