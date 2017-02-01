5c91160dfa34d683d82e104265bdf60fc67cda38
apple <1>
2017-02-01 10:53:39 +0200

    Create 02. Parse URLs

--- src/org/lynxlake/_06ManualStringProcessingLab/_01StudentsResults/Main.java
+++ src/org/lynxlake/_06ManualStringProcessingLab/_01StudentsResults/Main.java
@@ -5,8 +5,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
--- src/org/lynxlake/_06ManualStringProcessingLab/_02ParseURLs/Main.java
+++ src/org/lynxlake/_06ManualStringProcessingLab/_02ParseURLs/Main.java
@@ -1,7 +1,32 @@
+// https://judge.softuni.bg/Contests/Practice/Index/457#1
 package org.lynxlake._06ManualStringProcessingLab._02ParseURLs;
 
-/**
- * Created by Plamen Markov on 1/31/17.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String url = reader.readLine().trim();
+
+        String protocol;
+        String server;
+        String resource;
+
+        String[] tokens = url.split("://");
+        if (tokens.length != 2) {
+            System.out.println("Invalid URL");
+            return;
+        }
+
+        protocol = tokens[0];
+        int serverLastIdx = tokens[1].indexOf("/");
+        server = tokens[1].substring(0, serverLastIdx);
+        resource = tokens[1].substring(serverLastIdx + 1);
+
+        System.out.printf("Protocol = %s%n", protocol);
+        System.out.printf("Server = %s%n", server);
+        System.out.printf("Resources = %s%n", resource);
+    }
 }
