a9865ffa1053b7b68f0868681ca0fa8305469afc
apple <1>
2017-02-01 23:48:34 +0200

    Create 02. String Length

--- src/org/lynxlake/_06ManualStringProcessingExercises/_02StringLength/Main.java
+++ src/org/lynxlake/_06ManualStringProcessingExercises/_02StringLength/Main.java
@@ -1,7 +1,22 @@
+// https://judge.softuni.bg/Contests/Practice/Index/195#1
 package org.lynxlake._06ManualStringProcessingExercises._02StringLength;
 
-/**
- * Created by Plamen Markov on 2/1/17.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input = reader.readLine();
+        int len = 20;
+
+        StringBuilder sb = new StringBuilder(input);
+        if (sb.toString().length() < len) {
+            String s = "*";
+            sb.append(new String(new char[len - input.length()]).replace("\0", s));
+        }
+
+        System.out.println(sb.toString().substring(0, len));
+    }
 }
