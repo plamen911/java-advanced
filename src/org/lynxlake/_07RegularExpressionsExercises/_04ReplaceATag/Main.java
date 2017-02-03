3c55a3f8944a283416a4f46328e5fb5c51441b1c
apple <1>
2017-02-03 14:06:45 +0200

    Create 04. Replace a Tag

--- src/org/lynxlake/_07RegularExpressionsExercises/_04ReplaceATag/Main.java
+++ src/org/lynxlake/_07RegularExpressionsExercises/_04ReplaceATag/Main.java
@@ -1,5 +1,5 @@
 // https://judge.softuni.bg/Contests/Practice/Index/189#3
-package org.lynxlake._07RegularExpressionsExercises.replaceTag;
+package org.lynxlake._07RegularExpressionsExercises._04ReplaceATag;
 
 import java.io.BufferedReader;
 import java.io.IOException;
