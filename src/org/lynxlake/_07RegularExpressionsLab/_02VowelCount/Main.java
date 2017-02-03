7ce5a784f8df5a0414634353a04293ef4ac053c9
apple <1>
2017-02-03 12:31:29 +0200

    Create 02. Vowel Count

--- src/org/lynxlake/_07RegularExpressionsLab/_02VowelCount/Main.java
+++ src/org/lynxlake/_07RegularExpressionsLab/_02VowelCount/Main.java
@@ -1,7 +1,25 @@
+// https://judge.softuni.bg/Contests/Practice/Index/458#1
 package org.lynxlake._07RegularExpressionsLab._02VowelCount;
 
-/**
- * Created by Plamen Markov on 2/3/17.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String text = reader.readLine();
+
+        Pattern pattern = Pattern.compile("[AEIOUYaeiouy]");
+        Matcher matcher = pattern.matcher(text);
+
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+
+        System.out.println("Vowels: " + count);
+    }
 }
