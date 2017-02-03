31ed7724046db6fac02321fc5eec5fdfe1b0d85e
apple <1>
2017-02-03 14:05:50 +0200

    Create 03. Series Of Letters

--- src/org/lynxlake/_07RegularExpressionsExercises/_03SeriesOfLetters/Main.java
+++ src/org/lynxlake/_07RegularExpressionsExercises/_03SeriesOfLetters/Main.java
@@ -1,5 +1,5 @@
 // https://judge.softuni.bg/Contests/Practice/Index/189#2
-package org.lynxlake._07RegularExpressionsExercises.seriesOfLetters;
+package org.lynxlake._07RegularExpressionsExercises._03SeriesOfLetters;
 
 import java.io.BufferedReader;
 import java.io.IOException;
