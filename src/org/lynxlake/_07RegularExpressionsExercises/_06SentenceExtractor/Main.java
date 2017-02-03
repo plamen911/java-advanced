a5aba6574e989fedb3169c27278bca3f2012a1c7
apple <1>
2017-02-03 14:09:26 +0200

    Create 06. Sentence Extractor

--- src/org/lynxlake/_07RegularExpressionsExercises/_06SentenceExtractor/Main.java
+++ src/org/lynxlake/_07RegularExpressionsExercises/_06SentenceExtractor/Main.java
@@ -1,5 +1,5 @@
 // https://judge.softuni.bg/Contests/Practice/Index/189#5
-package org.lynxlake._07RegularExpressionsExercises.sentenceExtractor;
+package org.lynxlake._07RegularExpressionsExercises._06SentenceExtractor;
 
 import java.io.BufferedReader;
 import java.io.IOException;
