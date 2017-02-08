2930385561575da871d91a15254e30979f0a2973
apple <1>
2017-02-08 14:36:59 +0200

    Create 02. SumNumbers

--- src/org/lynxlake/_08FunctionalProgramingLab/_02SumNumbers/Main.java
+++ src/org/lynxlake/_08FunctionalProgramingLab/_02SumNumbers/Main.java
@@ -1,7 +1,24 @@
+// https://judge.softuni.bg/Contests/Practice/Index/463#1
 package org.lynxlake._08FunctionalProgramingLab._02SumNumbers;
 
-/**
- * Created by Plamen Markov on 2/8/17.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.function.Function;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = reader.readLine().split(", ");
+
+        Function<String, Integer> parser = Integer::parseInt;
+
+        int sum = 0;
+        for (String s : input) {
+            sum += parser.apply(s);
+        }
+
+        System.out.println("Count = " + input.length);
+        System.out.println("Sum = " + sum);
+    }
 }
