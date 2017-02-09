544dff05e2f1fae78c10090a586625ce22b0335c
apple <1>
2017-02-09 19:31:56 +0200

    PredicateParty moved to Exercise pachage

--- src/org/lynxlake/_08FunctionalProgrammingExercises/_11PredicateParty/Main.java
+++ src/org/lynxlake/_08FunctionalProgrammingExercises/_11PredicateParty/Main.java
@@ -1,5 +1,5 @@
 // https://judge.softuni.bg/Contests/Practice/Index/194#10
-package org.lynxlake._08FunctionalProgramingLab._11PredicateParty;
+package org.lynxlake._08FunctionalProgrammingExercises._11PredicateParty;
 
 import java.io.BufferedReader;
 import java.io.IOException;
