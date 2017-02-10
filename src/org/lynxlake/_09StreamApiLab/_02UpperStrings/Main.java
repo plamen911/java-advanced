a4c6cac428c0ceef731bf99d553beec2086cbe4a
apple <1>
2017-02-10 18:04:36 +0200

    Create 02. Upper Strings

--- src/org/lynxlake/_09StreamApiLab/_02UpperStrings/Main.java
+++ src/org/lynxlake/_09StreamApiLab/_02UpperStrings/Main.java
@@ -1,7 +1,16 @@
+// https://judge.softuni.bg/Contests/Practice/Index/465#1
 package org.lynxlake._09StreamApiLab._02UpperStrings;
 
-/**
- * Created by Plamen Markov on 2/10/17.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Arrays.stream(reader.readLine().split("\\s+"))
+                .map(String::toUpperCase)
+                .forEach(b -> System.out.print(b + " "));
+    }
 }
