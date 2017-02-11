0c6b4b42f03912d296f55919150f6f332e05c429
apple <1>
2017-02-11 16:18:06 +0200

    Create 08. Bounded Numbers

--- src/org/lynxlake/_09StreamApiLab/_08BoundedNumbers/Main.java
+++ src/org/lynxlake/_09StreamApiLab/_08BoundedNumbers/Main.java
@@ -1,8 +1,24 @@
 // https://judge.softuni.bg/Contests/Practice/Index/465#7
 package org.lynxlake._09StreamApiLab._08BoundedNumbers;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] bounds = Arrays.stream(reader.readLine().split("\\s+"))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+        int lowerBound = Math.min(bounds[0], bounds[1]);
+        int upperBound = Math.max(bounds[0], bounds[1]);
+
+        Arrays.stream(reader.readLine().split("\\s+"))
+                .filter(a -> !a.isEmpty())
+                .mapToInt(Integer::valueOf)
+                .filter(n -> lowerBound <= n && n <= upperBound)
+                .forEach(e -> System.out.print(e + " "));
     }
 }
