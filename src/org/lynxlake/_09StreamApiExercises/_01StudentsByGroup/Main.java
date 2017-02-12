5709b08755b7db89e50d73bbfb653a056f16f3cb
apple <1>
2017-02-12 08:41:07 +0200

    Create 01. Students by Group

--- src/org/lynxlake/_09StreamApiExercises/_01StudentsByGroup/Main.java
+++ src/org/lynxlake/_09StreamApiExercises/_01StudentsByGroup/Main.java
@@ -1,5 +1,6 @@
 package org.lynxlake._09StreamApiExercises._01StudentsByGroup;
 
+import org.lynxlake._09StreamApiExercises.Helper;
 import org.lynxlake._09StreamApiExercises.Student;
 
 import java.io.IOException;
@@ -7,42 +8,16 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-/*        Path studentDataPath = Paths.get("src/resources/StudentData.txt");
-        List<String> data = new ArrayList<>();
-
-        try {
-            data = Files.readAllLines(studentDataPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        List<Student> students = new ArrayList<>();
-        for (int i = 1; i < data.size(); i++) {
-            String[] tokens = data.get(i).split("\\s+");
-            String facNum = tokens[0];
-            String firstName = tokens[1];
-            String lastName = tokens[2];
-            String email = tokens[3];
-            Integer age = Integer.parseInt(tokens[4]);
-            Integer group = Integer.parseInt(tokens[5]);
-            String phone = tokens[10];
-
-            Student student = new Student(facNum, firstName, lastName, email, age, group, phone);
-
-            for (int gradeIndex = 6; gradeIndex <= 9; gradeIndex++) {
-                Integer grade = Integer.valueOf(tokens[gradeIndex]);
-                student.getGrades().add(grade);
-            }
-
-            students.add(student);
-        }
+        List<Student> students = Helper.getStudents();
 
         students.stream()
-                .filter(student -> student.getFirstName().compareTo(student.getLastName()) < 0)
-                .forEach(student -> System.out.println(student.getFirstName() + " " + student.getLastName()));*/
+                .filter(a -> a.getGroup() == 2)
+                .sorted(Comparator.comparing(Student::getFirstName))
+                .forEach(a -> System.out.println(a.getFirstName() + " " + a.getLastName()));
     }
 }
