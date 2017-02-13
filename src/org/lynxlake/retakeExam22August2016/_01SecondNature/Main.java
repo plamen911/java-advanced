e808f3601d3309a3bcc134fc329fb6e75f4ae13e
apple <1>
2017-02-14 00:39:59 +0200

    Create 01. Second Nature

--- src/org/lynxlake/retakeExam22August2016/_01SecondNature/Main.java
+++ src/org/lynxlake/retakeExam22August2016/_01SecondNature/Main.java
@@ -4,31 +4,64 @@ package org.lynxlake.retakeExam22August2016._01SecondNature;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> flowers = new ArrayList<>();
-        ArrayList<Integer> buckets = new ArrayList<>();
-        String[] input = reader.readLine().trim().split("\\s+"); // flowers, and the Weiss dust in them
-        for (String anInput1 : input) {
-            flowers.add(Integer.parseInt(anInput1));
-        }
-        input = reader.readLine().trim().split("\\s+"); // buckets with water
-        for (String anInput : input) {
-            buckets.add(Integer.parseInt(anInput));
+        List<Integer> flowers = Arrays.stream(reader.readLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        List<Integer> buckets = Arrays.stream(reader.readLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        Collections.reverse(buckets);
+
+        List<Integer> secondNature = new LinkedList<>();
+
+        while (flowers.size() > 0 && buckets.size() > 0) {
+            int currentIdx = 0;
+            int nextIdx = currentIdx + 1;
+            int flower = flowers.get(currentIdx);
+            int bucket = buckets.get(currentIdx);
+
+            if (bucket > flower) {
+                flowers.remove(currentIdx);
+                if (nextIdx >= buckets.size()) {
+                    buckets.set(currentIdx, buckets.get(currentIdx) + bucket - flower);
+                    break;
+                }
+                buckets.set(nextIdx, buckets.get(nextIdx) + bucket - flower);
+                buckets.remove(currentIdx);
+
+            } else if (bucket < flower) {
+                buckets.remove(currentIdx);
+                flowers.set(currentIdx, flower - bucket);
+
+            } else {
+                secondNature.add(flowers.get(currentIdx));
+                flowers.remove(currentIdx);
+                buckets.remove(currentIdx);
+            }
         }
 
-        int i = buckets.size() - 1;
-        for (Integer flower : flowers) {
-            int rest = buckets.get(i) - flower;
-            System.out.println(rest + " = " + buckets.get(i) + " - " + flower);
-            i--;
-            buckets.set(i, buckets.get(i) + rest);
+        if (buckets.size() > 0) {
+            joinListOfIntegers(buckets);
+        } else if (flowers.size() > 0) {
+            joinListOfIntegers(flowers);
+        } else {
+            System.out.println("None");
         }
 
-        buckets.forEach(System.out::println);
+        if (secondNature.size() > 0) {
+            joinListOfIntegers(secondNature);
+        } else {
+            System.out.println("None");
+        }
+    }
 
+    private static void joinListOfIntegers(List<Integer> buckets) {
+        System.out.printf("%s%n", String.join(" ", buckets.stream().map(String::valueOf).collect(Collectors.toList())));
     }
 }
