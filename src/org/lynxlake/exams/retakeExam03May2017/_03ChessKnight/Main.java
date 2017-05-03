90e02e7d29acf1f75ed5ca8d8ae15ef737ea205d
apple <1>
2017-05-03 12:05:51 +0300

    04.FootballStats ready

--- src/org/lynxlake/exams/retakeExam03May2017/_03ChessKnight/Main.java
+++ src/org/lynxlake/exams/retakeExam03May2017/_03ChessKnight/Main.java
@@ -1,11 +1,68 @@
 // https://judge.softuni.bg/Contests/Compete/Index/549#2
 package org.lynxlake.exams.retakeExam03May2017._03ChessKnight;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Plamen Markov on 5/3/17.
  */
 public class Main {
-    public static void main(String[] args) {
-        
+    private static final int chessSize = 8;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[][] matrix = new String[chessSize][chessSize];
+        List<String> piecesTaken = new ArrayList<>();
+        int invalidMoves = 0;
+        int movesOutOfBoard = 0;
+
+        // fill-in the chess board
+        for (int i = 0; i < chessSize; i++) {
+            String[] params = reader.readLine().trim().split("\\|");
+            System.arraycopy(params, 0, matrix[i], 0, chessSize);
+        }
+
+        String startPosition = reader.readLine();
+        int row = Integer.parseInt(startPosition.substring(0, 1));
+        int col = Integer.parseInt(startPosition.substring(1));
+
+        String line = "";
+
+        while (!"END".equals(line = reader.readLine())) {
+            String[] params = line.split(" -> ");
+
+            int startRow = Integer.parseInt(params[0].substring(0, 1));
+            int startCol = Integer.parseInt(params[0].substring(1));
+
+            int endRow = Integer.parseInt(params[1].substring(0, 1));
+            int endCol = Integer.parseInt(params[1].substring(1));
+
+            if (!isValidMove(startRow, startCol, endRow, endCol)) {
+                invalidMoves++;
+            } else if (isOutOfBoard(endRow, endCol)) {
+                movesOutOfBoard++;
+            } else if (!matrix[endRow][endCol].equals(" ")) {
+                piecesTaken.add(matrix[endRow][endCol]);
+            }
+        }
+
+        System.out.printf("Pieces take: %s%n", String.join(", ", piecesTaken));
+        System.out.printf("Invalid moves: %d%n", invalidMoves);
+        System.out.printf("Board out moves: %d%n", movesOutOfBoard);
+    }
+
+    private static boolean isOutOfBoard(int row, int col) {
+        return (row >= chessSize || col >= chessSize);
+    }
+
+    private static boolean isValidMove(int r1, int c1, int r2, int c2) {
+        int row = Math.abs(r2 - r1);
+        int col = Math.abs(c2 - c1);
+        return ((row == 2 && col == 1) || (row == 1 && col == 2));
     }
 }
+
--- src/org/lynxlake/exams/retakeExam03May2017/_04FootballStats/Main.java
+++ src/org/lynxlake/exams/retakeExam03May2017/_04FootballStats/Main.java
@@ -1,11 +1,91 @@
 // https://judge.softuni.bg/Contests/Compete/Index/549#3
 package org.lynxlake.exams.retakeExam03May2017._04FootballStats;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Plamen Markov on 5/3/17.
  */
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = "";
+        Map<String, List<Game>> teamList = new HashMap<>();
+
+        final String regex = "^(.*?)\\s-\\s+(.*?)\\sresult\\s(\\d):(\\d+)$";
+        final Pattern pattern = Pattern.compile(regex);
+
+        while (!"Season End".equals(line = reader.readLine())) {
+            final Matcher matcher = pattern.matcher(line);
+
+            while (matcher.find()) {
+                String firstTeam = matcher.group(1);
+                String secondTeam = matcher.group(2);
+                int firstTeamGoals = Integer.parseInt(matcher.group(3));
+                int secondTeamGoals = Integer.parseInt(matcher.group(4));
+
+                Game firstTeamGame = new Game(firstTeam, secondTeam, firstTeamGoals, secondTeamGoals);
+                Game secondTeamGame = new Game(secondTeam, firstTeam, secondTeamGoals, firstTeamGoals);
+
+                teamList.putIfAbsent(firstTeam, new ArrayList<>());
+                teamList.putIfAbsent(secondTeam, new ArrayList<>());
+
+                teamList.get(firstTeam).add(firstTeamGame);
+                teamList.get(secondTeam).add(secondTeamGame);
+            }
+        }
+
+        String[] teamsToPrint = reader.readLine().trim().split(",\\s+");
+
+        for (String teamName : teamsToPrint) {
+            if (teamList.containsKey(teamName)) {
+                List<Game> games = teamList.get(teamName);
+
+                games.stream()
+                        .sorted(Comparator.comparing(Game::getSecondTeam))
+                        .forEach(game -> {
+                            System.out.printf("%s - %s -> %d:%d%n",
+                                    game.getFirstTeam(),
+                                    game.getSecondTeam(),
+                                    game.getFirstTeamGoals(),
+                                    game.getSecondTeamGoals());
+                        });
+            }
+        }
+    }
+}
+
+class Game {
+    private String firstTeam;
+    private String secondTeam;
+    private int firstTeamGoals;
+    private int secondTeamGoals;
+
+    public Game(String firstTeam, String secondTeam, int firstTeamGoals, int secondTeamGoals) {
+        this.firstTeam = firstTeam;
+        this.secondTeam = secondTeam;
+        this.firstTeamGoals = firstTeamGoals;
+        this.secondTeamGoals = secondTeamGoals;
+    }
+
+    public String getFirstTeam() {
+        return this.firstTeam;
+    }
+
+    public String getSecondTeam() {
+        return this.secondTeam;
+    }
+
+    public int getFirstTeamGoals() {
+        return this.firstTeamGoals;
+    }
+
+    public int getSecondTeamGoals() {
+        return this.secondTeamGoals;
     }
 }
